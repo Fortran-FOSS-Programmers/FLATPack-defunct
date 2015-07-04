ed10836e7cc5d032e11a37c8bc22f37947a024fe
Chris MacMackin <cmacmackin@gmail.com>
2015-07-03 21:33:51 -0300

    Refactored for tidier build process.

--- build.py
+++ build.py
@@ -1,4 +1,16 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, task, Author
+from codecs import open
+import re
+
+try:
+    import pandoc
+    import subprocess
+    pandoc.core.PANDOC_PATH = subprocess.Popen(["which pandoc"],shell=True,stdout=subprocess.PIPE).communicate()[0][:-1]
+    doc = pandoc.Document()
+    doc.markdown = open('README.md').read()
+    long_description = doc.rst
+except:
+    long_description = open('README.md').read()
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -7,11 +19,48 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
+__source__ = open('src/main/python/flatpack/__init__.py').read()
 
-name = "FLATPack"
+authors = [Author(re.search(r'^__author__\s*=\s*"(.*)"', __source__, re.M).group(1),
+                  re.search(r'^__email__\s*=\s*"(.*)"', __source__, re.M).group(1))]
+version = re.search(r'^__version__\s*=\s*"(.*)"', __source__, re.M).group(1)
+license = re.search(r'^__license__\s*=\s*"(.*)"', __source__, re.M).group(1)
+description = re.search(r'^__description__\s*=\s*"(.*)"', __source__, re.M).group(1)
+url = re.search(r'^__url__\s*=\s*"(.*)"', __source__, re.M).group(1)
+name = re.search(r'^__appname__\s*=\s*"(.*)"', __source__, re.M).group(1)
+
 default_task = "publish"
 
 
 @init
 def set_properties(project):
-    pass
+    project.summary = description
+    project.description = long_description
+    
+    project.build_depends_on('coverage')
+    project.build_depends_on('flake8')
+    project.build_depends_on('frosted')
+    project.build_depends_on('pylint')
+    project.build_depends_on('pylint')
+    project.build_depends_on('pyandoc')
+    
+    project.depends_on('click')
+    
+    project.set_property('distutils_classifiers', [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System',
+        'Topic :: System :: Software Distribution',
+        'License :: OSI Approved :: ' + license,
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ])
+    
+    project.set_property('coverage_break_build', False)
+    
--- src/main/python/flatpack/__init__.py
+++ src/main/python/flatpack/__init__.py
@@ -1,1 +1,41 @@
-"""Package defining the flatpack.py program"""
+#!/usr/bin/env python
+"""
+FLATPack, Fortran Library, Application, and Tool Packaging
+
+Main program driver. Parses CLI and calls appropriate methods to run
+the program.
+"""
+__appname__     = "FLATPack"
+__description__ = "FLATPack, Fortran Library, Application, and Tool Packaging"
+#__long_description__ = 
+__version__     = "0.0.1"
+__author__      = "Chris MacMackin"
+__email__       = "cmacmackin@gmail.com"
+__license__     = "GNU General Public License v3 (GPLv3)"
+__url__         = "https://github.com/Fortran-FOSS-Programmers/FLATPack"
+__maintainer__  = "Chris MacMackin"
+__status__      = "Production"
+
+
+import click
+from .commands.cleanup_cmd import cleanup_run
+from .commands.compile_cmd import compile_run
+from .commands.help_cmd import help_run
+from .commands.list_cmd import list_run
+from .commands.remove_cmd import remove_run
+from .commands.search_cmd import search_run
+from .commands.update_cmd import update_run
+from .commands.upgrade_cmd import upgrade_run
+
+@click.group()
+def main():
+    pass
+    
+main.add_command(cleanup_run, name='cleanup')
+main.add_command(compile_run, name='compile')
+main.add_command(help_run, name='help')
+main.add_command(list_run, name='list')
+main.add_command(remove_run, name='remove')
+main.add_command(search_run, name='search')
+main.add_command(update_run, name='update')
+main.add_command(upgrade_run, name='upgrade')
